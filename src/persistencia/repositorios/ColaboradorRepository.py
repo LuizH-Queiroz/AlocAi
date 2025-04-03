097863977d4f9a8d733e70d13c4b10aa6bf7f131
LuizH-Queiroz <luizh.keiroz@gmail.com>
2025-04-03 11:10:24 -0300

    repositorios

--- src/persistencia/repositorios/ColaboradorRepository.py
+++ src/persistencia/repositorios/ColaboradorRepository.py
@@ -5,28 +5,33 @@ from typing import List
 
 class ColaboradorRepository:
     _instancia      = None
-    _colaboradorDAO = None
-    _daoFactory     = None
+    _inicializado   = False
 
     def __new__(cls):
         if cls._instancia is None:
             cls._instancia = super().__new__(cls)  # Cria uma unica instancia
 
-            cls._daoFactory = DAOFactory()
-            cls._colaboradorDAO = cls._daoFactory.getColaboradorDAO()
         return cls._instancia
 
-    def createColaborador(self, idColaborador: int, colaborador: Colaborador):
-        pass
+    def __init__(self):
+        if not self._inicializado:
+            self._inicializado      = True
+
+            self._daoFactory        = DAOFactory()
+            self._colaboradorDAO    = self._daoFactory.getColaboradorDAO()
+            
+
+    def createColaborador(self, colaborador: Colaborador):
+        self._colaboradorDAO.create(colaborador)
 
     def deleteColaborador(self, id: int):
-        pass
+        self._colaboradorDAO.delete(id)
 
     def readColaborador(self, id: int) -> Colaborador:
-        pass
+        return self._colaboradorDAO.read(id)
 
     def readAllColaborador(self) -> List[Colaborador]:
-        pass
+        return self._colaboradorDAO.readAll()
 
     def updateColaborador(self, id: int, novosDados: Colaborador):
-        pass
+        self._colaboradorDAO.update(id, novosDados)
--- src/persistencia/repositorios/EscalaRepository.py
+++ src/persistencia/repositorios/EscalaRepository.py
@@ -5,28 +5,30 @@ from typing import List
 
 class EscalaRepository:
     _instancia      = None
-    _escalaDAO      = None
-    _daoFactory     = None
+    _inicializado   = False
 
     def __new__(cls):
         if cls._instancia is None:
             cls._instancia = super().__new__(cls)  # Cria uma unica instancia
 
-            cls._daoFactory = DAOFactory()
-            cls._escalaDAO = cls._daoFactory.getEscalaDAO()
         return cls._instancia
 
-    def createEscala(self, idEscala: int, escala: Escala):
-        pass
+    def __init__(self):
+        if not self._inicializado:
+            self._inicializado = True
 
-    def deleteEscala(self, id: int):
-        pass
+            self._daoFactory = DAOFactory()
+            self._escalaDAO = self._daoFactory.getEscalaDAO()
 
-    def readEscala(self, id: int) -> Escala:
-        pass
 
-    def readAllEscala(self) -> List[Escala]:
-        pass
+    def createEscala(self, escala: Escala):
+        self._escalaDAO.create(escala)
 
-    def updateEscala(self, id: int, novosDados: Escala):
-        pass
+    def deleteEscala(self):
+        self._escalaDAO.delete()
+
+    def readEscala(self) -> Escala:
+        return self._escalaDAO.read()
+
+    def updateEscala(self, novosDados: Escala):
+        self._escalaDAO.update(novosDados)
