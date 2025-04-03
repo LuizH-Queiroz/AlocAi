6cb3b4d4fdfea142f85551fb6ea6b5d6981975a0
maurocoimbra <maurocoimbrab@gmail.com>
2025-04-03 17:06:11 -0300

    esqueleto

--- src/relatorio/RelaorioTemplate.py
+++ src/relatorio/RelaorioTemplate.py
@@ -1,4 +1,6 @@
 from abc import ABC, abstractmethod
+from persistencia.DAOs.RAM_ColaboradorDAO import RAM_ColaboradorDAO
+from persistencia.DAOs.RAM_EscalaDAO import RAM_EscalaDAO
 
 class RelatorioTemplate(ABC):
     def gerar_relatorio(self):
@@ -6,13 +8,11 @@ class RelatorioTemplate(ABC):
         escala = self.get_escala_geral()
         self.imprime_relatorio(colaboradores, escala)
 
-    @abstractmethod
-    def get_colaboradores(self):
-        pass
+    def get_colaboradores(self, dao_colaboradores: RAM_ColaboradorDAO):
+        return dao_colaboradores.readAll()
 
-    @abstractmethod
-    def get_escala_geral(self):
-        pass
+    def get_escala_geral(self, dao_escala: RAM_EscalaDAO):
+        return dao_escala.read()
 
     @abstractmethod
     def imprime_relatorio(self, colaboradores, escala):
--- src/relatorio/RelatorioDocX.py
+++ src/relatorio/RelatorioDocX.py
@@ -1,17 +1,6 @@
 from RelaorioTemplate import RelatorioTemplate
 
 class RelatorioDocX(RelatorioTemplate):
-    def get_colaboradores(self):
-        # PEGA COLABORADORES
-        colaboradores = ["Carlos", "Fernanda", "Lucia"]
-        return colaboradores
-
-    def get_escala_geral(self):
-        # PEGA A ESCALA
-        escala = {"Carlos": "Turno A", "Fernanda": "Turno B", "Lucia": "Turno C"}
-        return escala
 
     def imprime_relatorio(self, colaboradores, escala):
         print("Gerando relatório DOCX...")
-        for colaborador in colaboradores:
-            print(f"{colaborador}: {escala[colaborador]}")
