78713cb98d78cf25772144e65c9874a77e005a6b
Luerson <luersonalbuquerque@gmail.com>
2025-04-03 13:12:46 -0300

    persistencia em master

--- src/old_files/Colaborador.py
+++
@@ -1,4 +0,0 @@
-class Colaborador:
-  def __init__(self, nome, numeroRegistro):
-    self.nome           = nome
-    self.numeroRegistro = numeroRegistro
--- src/old_files/utils.py
+++
@@ -1,6 +0,0 @@
-import os
-import time
-
-def clear_sleep(t=0.5):
-  time.sleep(t)
-  os.system('cls' if os.name == 'nt' else 'clear')
