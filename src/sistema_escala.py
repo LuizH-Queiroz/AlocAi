b3fa892dc6eec661ffbdd8ab65b9f856ed19120d
camilaeduardo <camilaaeduardo@gmail.com>
2025-04-03 23:09:56 -0300

    Sistema escala finalizado (primeira entrega)

--- src/sistema_escala.py
+++ src/sistema_escala.py
@@ -1,30 +1,31 @@
 from persistencia.persistenciaFactories.PersistenciaFactory import PersistenciaFactory
 from relatorio.RelatorioFactory import RelatorioFactory
 from solvers.AdapterFactory import AdapterFactory
+from ui.UIFactory import UIFactory
 
 
 class SistemaEscala:
     
-    def __init__(self):
-        # template_factory = TemplateFactory()
-        
+    def __init__(self):        
         self.repositorio_colaborador = PersistenciaFactory().getColaboradorRepository()
         self.relatorio_template = RelatorioFactory().generate_relatorio_CSV()
         self.repositorio_escala = PersistenciaFactory().getEscalaRepository()
-        # UI_show = UIInterface()
-        self.solver_adapter = AdapterFactory.generateSolverAdapterMIP()
+        self.solver_adapter = AdapterFactory().generateSolverAdapterMIP()
 
     def runSystem(self):
         while(True):
-            choice = input('Escolha que tela você deseja visualizar\nOpções: Main, Escala e CC\n--> ')
+            choice = input('Escolha que tela você deseja visualizar\nOpções: Main, Escala e CC\nDigite 0 para sair\n--> ')
 
             match choice.lower():
                 case 'main':
-                    tela = self.UI_show.generateMainUI()
+                    tela = UIFactory().generateMainUI()
                 case 'escala':
-                    tela = self.UI_show.generateEscalaUI()
+                    tela = UIFactory().generateEscalaUI()
                 case 'cc':
-                    tela = self.UI_show.generateCCUI()
+                    tela = UIFactory().generateCCUI()
+                case '0':
+                    print('Saindo do sistema...')
+                    break
                 case _:
                     print('Tela não existente!')
                     continue
@@ -32,7 +33,7 @@ class SistemaEscala:
             tela.show()
 
     def gerarRelatorio(self):
-        self.relatorio_template.gerarRelatorio()
+        self.relatorio_template.gerar_relatorio()
     
 sistema = SistemaEscala()
 sistema.runSystem()
