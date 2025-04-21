4e406ca19795c4d9792b4b8bb160209fa0934f1f
Luerson <luersonalbuquerque@gmail.com>
2025-04-21 15:42:11 -0300

    _escalaSystem implementado para sistema_escala

--- src/ui/EscalaUI.py
+++ src/ui/EscalaUI.py
@@ -1,8 +1,28 @@
 # src/ui/EscalaUI.py
 from .UIInterface import UIInterface
-
+import os
 
 class EscalaUI(UIInterface):
+
+    def __init__(self):
+        self._escala = None
+        self._conteudo = ""
+
+    def set_escala(self, escala):
+        self._escala = escala
+    
+    def set_conteudo(self, conteudo):
+        self._conteudo = conteudo
+
     def show(self):
         # Implementação para exibir a interface de Escala
-        print("Exibindo a interface de Escala")
+        os.system('clear')
+        print("\nInterface Escala")
+        print("back.    volta para escala antecessora")
+        print("forward. avança para escala sucessora")
+        print("export.  exporta a escala atual")
+        print("main.    volta para a tela principal")
+        print()
+        print(self._conteudo)
+
+        print(self._escala)
